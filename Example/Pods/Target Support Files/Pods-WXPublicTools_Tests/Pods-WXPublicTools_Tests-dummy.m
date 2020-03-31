3ecd42004c15159982d576e76234958bcb9147bd
zhuxuezhen@offcn.com <zhuxuezhen@offcn.com>
2020-04-01 00:39:53 +0800

    初始化项目

---
+++ Example/Pods/Target Support Files/Pods-WXPublicTools_Tests/Pods-WXPublicTools_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_WXPublicTools_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_WXPublicTools_Tests
+@end
---
+++ Example/Pods/Target Support Files/WXPublicTools/WXPublicTools-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_WXPublicTools : NSObject
+@end
+@implementation PodsDummy_WXPublicTools
+@end
